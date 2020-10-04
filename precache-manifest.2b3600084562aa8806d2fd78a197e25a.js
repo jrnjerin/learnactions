34f732f79288e55a0024b66465b1e4ca8b20c969
jrnjerin <jrnjerin@gmail.com>
2020-10-04 11:24:53 +0000

    Updates

---
+++ precache-manifest.2b3600084562aa8806d2fd78a197e25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ae91edb72ba4825c23ece1761b5da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f894a1e5aaa35b6b571",
+    "url": "/static/css/main.37dd8d1f.chunk.css"
+  },
+  {
+    "revision": "6cd861b3327f183ca946",
+    "url": "/static/js/2.8c59d56a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c59d56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f894a1e5aaa35b6b571",
+    "url": "/static/js/main.0415f8f5.chunk.js"
+  },
+  {
+    "revision": "c6939c041bec8762b9c0",
+    "url": "/static/js/runtime-main.ff331b66.js"
+  }
+]);
